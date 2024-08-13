17a4df231756ff96a2873f8e2d27fd49c1aac338
Maxence Lallemand <maxence.lallemand@edu.univ-fcomte.fr>
2024-08-13 22:57:09 +0200

    chore: add handler for recipient_type, move letterhead functions to utils

--- post.go
+++ post.go
@@ -1,97 +1,115 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"github.com/WiiLink24/nwc24"
-	"github.com/gin-gonic/gin"
-	"io"
-	"io/ioutil"
 	"log"
 	"net/mail"
-	"os"
-	"os/exec"
 	"strings"
+	"unicode/utf16"
+
+	"github.com/WiiLink24/nwc24"
+	"github.com/gin-gonic/gin"
 )
 
 var (
-	InsertMail = `INSERT INTO mail (snowflake, data, sender, recipient, is_sent) VALUES ($1, $2, $3, $4, false)`
+    InsertMail = `INSERT INTO mail (snowflake, data, sender, recipient, is_sent) VALUES ($1, $2, $3, $4, false)`
 )
 
 func SendMessage(c *gin.Context) {
-	//fetch the message from the form
-	subject := c.PostForm("subject")
-	message := c.PostForm("message_content")
-	recipient := c.PostForm("recipient")
-	attachment := c.PostForm("attachment")
+    //fetch the message from the form
+    recipient_type := c.PostForm("recipient_type")
+    subject := c.PostForm("subject")
+    message := c.PostForm("message_content")
+    recipient := c.PostForm("recipient")
+    /* attachment := c.PostForm("attachment")
 
-	//convert attachment to []byte
-	attachment_data := []byte(attachment)
+    //convert attachment to []byte
+    attachment_data := []byte(attachment) */
 
-	formatted_recipient := strings.ReplaceAll(recipient, "-", "")
+    conv_message := utf16.Encode([]rune(message))
+    message = nwc24.UTF16ToString(conv_message)
 
-	sender_address, err := mail.ParseAddress("w9999999900000000@rc24.xyz")
-	if err != nil {
-		fmt.Println(err)
-	}
+    formatted_recipient := strings.ReplaceAll(recipient, "-", "")
 
-	recipient_address, err := mail.ParseAddress("w" + formatted_recipient + "@rc24.xyz")
-	if err != nil {
-		fmt.Println(err)
-	}
+    sender_address, err := mail.ParseAddress("w9999999900000000@rc24.xyz")
+    if err != nil {
+        fmt.Println(err)
+    }
 
-	//initialize the message
-	data := nwc24.NewMessage(sender_address, recipient_address)
-	data.SetSubject(subject)
-	data.SetContentType(nwc24.MultipartMixed)
-	data.SetBoundary(generateBoundary())
-	data.SetTag("X-Wii-MB-NoReply", "1")
-	data.SetTag("X-Wii-AppId", "2-48414541-0001")
-	data.SetTag("X-Wii-Cmd", "00044001")
+    var recipient_address *mail.Address
 
-	//create the multipart
-	multipart := nwc24.NewMultipart()
+    if recipient == "" && recipient_type == "all" {
+        recipient_address, err = mail.ParseAddress("allusers@rc24.xyz")
+        if err != nil {
+            fmt.Println(err)
+        }
 
-	//now we append the data
-	multipart.SetText(message, nwc24.UTF16BE)
-	multipart.SetContentType(nwc24.PlainText)
+    } else if recipient != "" && recipient_type == "single" {
+        recipient_address, err = mail.ParseAddress("w" + formatted_recipient + "@rc24.xyz")
+        if err != nil {
+            fmt.Println(err)
+        }
+    } else {
+        fmt.Println("Invalid recipient type")
+    }
 
-	img_multipart := nwc24.NewMultipart()
-	img_multipart.AddFile("attachment", attachment_data, nwc24.Jpeg)
+    //initialize the message
+    data := nwc24.NewMessage(sender_address, recipient_address)
+    data.SetSubject(subject)
+    /* data.SetText(message, nwc24.UTF16BE) */
+    data.SetBoundary(generateBoundary())
+    data.SetContentType(nwc24.MultipartMixed)
+    data.SetTag("X-Wii-MB-NoReply", "1")
 
-	//add the multipart to the message
-	data.AddMultipart(multipart, img_multipart)
+    //create the multipart
+    multipart := nwc24.NewMultipart()
 
-	content, err := data.ToString()
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(content)
-	}
+    //now we append the data
+    multipart.SetText(message, nwc24.UTF16BE)
+    multipart.SetContentType(nwc24.PlainText)
+/* 
+    img_multipart := nwc24.NewMultipart()
+    img_multipart.AddFile("attachment", attachment_data, nwc24.Jpeg) */
 
-	// Fetch the message from the form only if letter and thumbnail are uploaded
-	letterFile, _, _ := c.Request.FormFile("letter")
-	thumbnailFile, _, _ := c.Request.FormFile("thumbnail")
+    //add the multipart to the message
+    data.AddMultipart(multipart)
 
-	if letterFile != nil || thumbnailFile != nil {
-		uploadToGenerator(c, "letter", "letter.png")
-		uploadToGenerator(c, "thumbnail", "thumbnail.png")
+    content, err := nwc24.CreateMessageToSend(generateBoundary(), data)
+    if err != nil {
+        fmt.Println(err)
+    } else {
+        fmt.Println(content)
+    }
 
-		audioFile, _, _ := c.Request.FormFile("audio")
-		if audioFile != nil {
-			uploadToGenerator(c, "audio", "sound.wav")
-		}
-		letterheadContent, _ := generateLetterhead()
+    // Fetch the message from the form only if letter and thumbnail are uploaded
+    letterFile, _ := c.FormFile("letter")
+    thumbnailFile, _ := c.FormFile("thumbnail")
+
+    if letterFile != nil || thumbnailFile != nil {
+        uploadToGenerator(c, "letter", "letter.png")
+        uploadToGenerator(c, "thumbnail", "thumbnail.png")
+
+        audioFile, _, _ := c.Request.FormFile("audio")
+        if audioFile != nil {
+            uploadToGenerator(c, "audio", "sound.wav")
+        }
+        letterheadContent, _ := generateLetterhead()
 		log.Printf("Letterhead content: %s", letterheadContent)
-	} else {
-		fmt.Println("No letter or thumbnail uploaded, skipping...")
-	}
+    } else {
+        fmt.Println("No letter or thumbnail uploaded, skipping...")
+    }
 
-	_, err = wiiMailPool.Exec(ctx, InsertMail, flakeNode.Generate(), content, "9999999900000000", formatted_recipient)
-	if err != nil {
-		fmt.Println(err)
-	}
+    if recipient_type == "all" {
+        //insert null value for recipient
+        _, err = wiiMailPool.Exec(ctx, InsertMail, flakeNode.Generate(), content, "9999999900000000", "")
+    } else {
+        _, err = wiiMailPool.Exec(ctx, InsertMail, flakeNode.Generate(), content, "9999999900000000", formatted_recipient)
+    }
+    if err != nil {
+        fmt.Println(err)
+    }
 }
+
 
 func CheckInboundOutbound(c *gin.Context) {
 	/* number := c.Param("number") */
@@ -99,63 +117,4 @@ func CheckInboundOutbound(c *gin.Context) {
 
 func ClearInbound(c *gin.Context) {
 
-}
-
-func uploadToGenerator(c *gin.Context, source string, destination string) {
-	file, header, err := c.Request.FormFile(source)
-	if err != nil {
-		log.Printf("Failed to get uploaded file: %v", err)
-		return
-	}
-	defer file.Close()
-
-	// Get the filename
-	filename := header.Filename
-	fmt.Println("Received file:", filename)
-
-	// Ensure the generator/input directory exists
-	err = os.MkdirAll("generator/input", os.ModePerm)
-	if err != nil {
-		log.Printf("Failed to create directory: %v", err)
-		return
-	}
-
-	// Create a file for the letterhead
-	out, err := os.Create("generator/input/" + destination)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer out.Close()
-
-	// Write the uploaded file to the created file
-	_, err = io.Copy(out, file)
-	if err != nil {
-		log.Printf("Failed to write letterhead data: %v", err)
-	}
-}
-
-func generateLetterhead() (string, error) {
-	var out bytes.Buffer
-	var stderr bytes.Buffer
-
-	// Run generate.sh
-	cmd := exec.Command("sh", "generator/generate.sh")
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-
-	log.Printf("Running command and waiting for it to finish...")
-	err := cmd.Run()
-	if err != nil {
-		return "", fmt.Errorf("failed to run generate.sh: %v: %s", err, stderr.String())
-	}
-
-	// Read the data from generator/output/letterhead.txt
-	letterheadData, err := ioutil.ReadFile("generator/output/letterhead.txt")
-	if err != nil {
-		return "", fmt.Errorf("failed to read file: %v", err)
-	}
-
-	// Convert the data to a string and store it in a variable
-	letterheadContent := string(letterheadData)
-	return letterheadContent, nil
 }
